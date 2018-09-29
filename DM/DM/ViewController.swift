3d2b2f96c2803bbe6a5b4f64c4ba9c6b3d14de55
TigranTT <tigrantt@yahoo.com>
2018-09-28 23:28:54 -0500

    first commit

---
+++ DM/DM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DM
+//
+//  Created by Tigran Tshorokhyan on 9/28/18.
+//  Copyright © 2018 Tigran Tshorokhyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
